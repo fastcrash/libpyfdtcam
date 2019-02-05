8ee68f8a29b0cbcb6686240356bd8c188a861dd8
fastcrash <losse83@posteo.de>
2019-02-05 17:39:47 +0100

    Version 1.0

--- libpyfdtcam/libpyfdtcam.py
+++ libpyfdtcam/libpyfdtcam.py
@@ -1,9 +1,13 @@
+""""
+A small Library for FDT IP-Cams.
+"""
+
 import requests
 
 class FDTCam(object):
-    def __init__(self, host, port, user, password):
+    def __init__(self, host, port, username, password):
         self._host = host
-        self._user = user
+        self._username = username
         self._password = password
         self._port = port
         self.session = requests.Session()
@@ -15,14 +19,14 @@ class FDTCam(object):
         """Base URL used CGI API requests on FDT Camera."""
         return "http://" + self._host + \
                "/cgi-bin/hi3510/param.cgi?cmd={}&-usr=" + \
-               self._user + "&-pwd=" + self._password
+               self._username + "&-pwd=" + self._password
 
     @property
     def __command_url(self):
         """Base command URL used by CGI API requests."""
         return "http://" + self._host + \
                "/cgi-bin/hi3510/{}&-usr=" + \
-               self._user + "&-pwd=" + self._password
+               self._username + "&-pwd=" + self._password
 
     def query(self, cmd, raw=False):
         """Generic abstraction to run query."""
@@ -33,14 +37,21 @@ class FDTCam(object):
 
         return req.text if raw else self.to_dict(req.text)
 
-    def send(self, cmd, payload=None, raw=False):
+    def send(self, cmd, payload=None, raw=False, callback=False):
+        """Sends Command via CGI. Payload has to be dictionary with 'parameter:value' format."""
+        # Build URL for command.
         url = self.__baseurl.format(cmd)
+        # Add payload to url.
         for k, v in payload.items():
             url += "&-" + k + "=" + v
-        print(url)
+
         req = self.session.get(url)
+
         if not req.ok:
             req.raise_for_status()
+
+        if callback:
+            return req.text if raw else self.to_dict(req.text)
 
     @property
     def device_type(self):
@@ -48,17 +59,14 @@ class FDTCam(object):
         return self.query('getdevtype').get('devtype')
 
 
-    def get_snapshot(self, filename=None):
+    def get_snapshot(self):
         """Return camera snapshot."""
         url = self.__command_url.format('web/tmpfs/auto.jpg')
 
         req = self.session.get(url)
         if req.ok:
-            if filename is None:
-                return req.content
+            return req.content
 
-            with open(filename, 'wb') as snap:
-                snap.write(req.content)
         return req
 
     @property
@@ -80,52 +88,68 @@ class FDTCam(object):
 
     @ir_status.setter
     def ir_status(self, status):
+        """ Set status of IR-LED """
         payload = {"infraredstat":status}
         self.send('setinfrared', payload)
 
-    @property
-    def ptz_preset(self):
-        return
+    def ir_on(self):
+        self.send("setinfrared", 1)
+        """ Switch IR-LED on """
 
-    @ptz_preset.setter
+    def ir_off(self):
+        """ Switch IR-LED off """
+        self.send("setinfrared", 0)
+
+    
     def ptz_preset(self, preset):
+        """ Move cam to PTZ preset."""
         preset -= 1
         preset = str(preset)
         payload = {"act": "goto", "number": preset}
         self.send('preset', payload)
 
     def ptz_control(self, act, speed, step=1):
+        """ Send PTZ control command."""
         payload = {"act": act, "speed": str(speed), "step": str(step)}
         self.send('ptzctrl', payload)
 
     def ptz_up(self):
+        """ Move cam up. """
         self.ptz_control("up", 45, 0)
 
     def ptz_down(self):
+        """ Move cam down. """
         self.ptz_control("down", 45, 0)
 
     def ptz_left(self):
+        """ Move cam left. """
         self.ptz_control("left", 45, 0)
 
     def ptz_right(self):
+        """ Move cam right. """
         self.ptz_control("right", 45, 0)
 
     def ptz_stop(self):
+        """ Stop PTZ. """
         self.ptz_control("stop", 45, 0)
 
     @property
     def motion_detect(self):
+        """ Get status of Motion Detection. """
         return bool(self.query("getmdattr").get("m1_enable"))
 
     @motion_detect.setter
     def motion_detect(self, status, area=1, sens=50):
+        """ Set status of Motion Detection """
         payload = {"enable": str(status), "area": str(area), "s": str(sens)}
         self.send("setmdattr", payload)
 
     def motion_on(self):
+        """ Activate Motion Detection. """
         self.motion_detect(1)
 
     def motion_off(self):
+        """ Deactivate Motion Detection. """
         self.motion_detect(0)
 
     def to_dict(self, response):
